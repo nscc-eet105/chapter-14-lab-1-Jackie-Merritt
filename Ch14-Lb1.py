f5b6a41383e49fdaa53445d7c2300af5d8fe53b3
Jackie Merritt <remmerritt@gmail.com>
2025-07-13 22:54:43 -0400

    Help

---
+++ Ch14-Lb1.py
@@ -0,0 +1,26 @@
+#Jackie-Merritt_Chp14-Lab1_7/13/2025
+import dog
+import vehicle as v
+
+def main():
+    Car = v(accelerating)
+    Car.accelerate()
+    Car.display_speed()
+
+
+
+
+
+
+def test():
+
+    dog1 = dog.Dog('Fido', 3, 'mutt')
+    dog2 = dog.Dog('Lassie', 5, 'Collie')
+
+    print(dog1)
+    print(dog2)
+
+main()
+
+
+
