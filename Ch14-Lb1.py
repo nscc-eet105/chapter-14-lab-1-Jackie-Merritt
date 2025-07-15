7c6d019a6f9625094a42abf3a1fc8d672c2d7b5c
Jackie Merritt <remmerritt@gmail.com>
2025-07-15 16:02:34 -0400

    Assignment Completed

--- Ch14-Lb1.py
+++ Ch14-Lb1.py
@@ -1,24 +1,33 @@
 #Jackie-Merritt_Chp14-Lab1_7/13/2025
-import dog
-import vehicle as v
+#import vehicle as v
+class V:
+    def __init__(self):
+        self.__current_speed = 0
+        
+
+    def decelerate(self):
+        print("Decelerating...")
+        self.__current_speed -= 1
+
+    def accelerate(self):
+        print("Accelerating...")
+        self.__current_speed += 1
+
+    def display_speed(self):
+        print(f'Current speed: {self.__current_speed}')
 
 def main():
-    Car = v(accelerating)
-    Car.accelerate()
-    Car.display_speed()
+    Vehicle = V()
+    Vehicle.accelerate()
+    Vehicle.display_speed()
+    Vehicle.accelerate()
+    Vehicle.display_speed()
+    print()
+    Vehicle.decelerate()
+    Vehicle.display_speed()
+    Vehicle.decelerate()
+    Vehicle.display_speed()
 
-
-
-
-
-
-def test():
-
-    dog1 = dog.Dog('Fido', 3, 'mutt')
-    dog2 = dog.Dog('Lassie', 5, 'Collie')
-
-    print(dog1)
-    print(dog2)
 
 main()
 
